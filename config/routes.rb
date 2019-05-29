b7a3dadd6e11e839051d96db0fa2a0f16fe1e1e0
Dareth NHANG <contact@dsigned.me>
2019-05-29 13:35:51 +0200

    WIP: Dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   
+  # TASKS
   resources :tasks, only: [:index, :show, :new, :edit, :create, :update] do
     resources :assignments, only: [ :index, :new, :create ]
   end
   
+  # ASSIGNMENTS
   resources :assignments, only: [ :show, :edit, :update, :destroy ]  do
   	member do
       patch 'valid'
@@ -11,13 +13,17 @@ Rails.application.routes.draw do
   	end
   end
   
+  # CUSTOM PAGES
   get 'my-tasks', to: 'tasks#mytasks'
   get 'dashboard', to: 'pages#dashboard'
 
+  # USERS
   # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root to: 'pages#dashboard'
+  
+  # HOME
+  root to: 'pages#home'
 
 end
