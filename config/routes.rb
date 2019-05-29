e9e8ae1d15ef472526d10ded39856fca05e384ed
Pierre M'baga <pierrembaga@gmail.com>
2019-05-29 17:36:41 +0200

    ADD: Forest-Connect gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  mount ForestLiana::Engine => '/forest'
   # FLATS
   resources :flats
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_22_165130) do
+ActiveRecord::Schema.define(version: 2019_05_28_154929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,14 +96,14 @@ ActiveRecord::Schema.define(version: 2019_05_22_165130) do
     t.string "first_name"
     t.string "last_name"
     t.string "role"
-    t.string "phone"
-    t.date "birthdate"
-    t.string "sex"
     t.string "provider"
     t.string "uid"
     t.string "facebook_picture_url"
     t.string "token"
     t.datetime "token_expiry"
+    t.string "phone"
+    t.date "birthdate"
+    t.string "sex"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
