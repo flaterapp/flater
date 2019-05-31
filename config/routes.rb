351395d531c1a0cf7d27560f38c61c36a0ee24f2
Pierre M'baga <pierrembaga@gmail.com>
2019-05-31 11:19:36 +0200

    TWEAK: Sign-in now redirect to flats#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 Rails.application.routes.draw do
-  
+
   # FOREST ADMIN
   mount ForestLiana::Engine => '/forest'
-  
+
   # FLATS
   resources :flats, only: [:index, :show, :new, :edit, :create, :update] do
     resources :rentals, only: [:index, :show, :new, :edit, :create]
   end
-  
+
   # TASKS
   resources :tasks, only: [:index, :show, :new, :edit, :create, :update] do
     resources :assignments, only: [ :index, :new, :create, :confirmation ]
   end
-  
+
   # ASSIGNMENTS
   resources :assignments, only: [ :show, :edit, :update, :destroy ]  do
   	member do
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
       patch 'reject'
   	end
   end
-  
+
   # CUSTOM PAGES
   get 'dashboard', to: 'pages#dashboard'
   get 'my-tasks', to: 'tasks#mytasks'
@@ -29,9 +29,11 @@ Rails.application.routes.draw do
   # USERS
   # devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  
+
   # HOME
   root to: 'pages#home'
 
