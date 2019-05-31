6184d805df95c3517ecc7f77dd8c6934886c5791
Dareth NHANG <contact@dsigned.me>
2019-05-31 15:31:01 +0200

    WIP: Rental #show

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,27 +1,34 @@
 class RentalsController < ApplicationController
-	before_action :set_flat, only: [:new, :create]
+  before_action :set_flat, only: [:new, :create]
 
+  def show
+    @rental = Rental.find(params[:id])
+    @flat = Flat.find(params[:flat_id])
+  end
 
-	def show
-	  @rental = Rental.find(params[:id])
-	  @flat = Flat.find(params[:flat_id])
-	end
+  def new
+    @rental = Rental.new
+  end
 
-	def new
-	  @rental = Rental.new
-	end
+  def create
+    @rental = Rental.new(rental_params)
+    @rental.pending = true
+    @rental.flat = @flat
+    @rental.save!
+    # OPTIMIZE IF OK ... else ...
+  end
 
-	def create
-	  @rental = Rental.new(rental_params)
-	  @rental.pending = true
-	  @rental.flat = @flat
-	  @rental.save!
-	  # TO TWEAK: IF OK ... else ...
-	end
+  def my_applications
+    @pending_rentals = Rental.where(pending: true)
+  end
 
-	def my_applications
-	  @pending_rentals = Rental.where(pending: true)
-	end
+  def organize_visit
+    @rental = Rental.find(params[:id])
+    @flat = Rental.find(params[:flat_id])
+    @task = Task.new
+    @invited_participants = @rental.dossiers.where(status: "ok_for_visit")
+    @invited_participants.update(status: "visiting")
+  end
 
 	def organize_visit
 	  @rental = Rental.find(params[:id])
@@ -30,15 +37,14 @@ class RentalsController < ApplicationController
 	  @invited_participants = @rental.dossiers.where(status: "ok_for_visit")
 	  @invited_participants.update(status: "visiting")
 	end
+  private
 
-	private
+  def rental_params
+    params.require(:rental).permit(:description, :start_date, :end_date, :initial_rent)
+  end
 
-	def rental_params
-	  params.require(:rental).permit(:description, :start_date, :end_date, :initial_rent)
-	end
-
-	def set_flat
-	  @flat = Flat.find(params[:flat_id])
-	end
+  def set_flat
+    @flat = Flat.find(params[:flat_id])
+  end
 
 end
