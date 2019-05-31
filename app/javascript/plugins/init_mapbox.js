e0eeef9f88aa883af64dfd233012ddc5237f9e56
Dareth NHANG <contact@dsigned.me>
2019-05-31 15:28:40 +0200

    TWEAK: My flats #index

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -29,9 +29,8 @@ const initMapbox = () => {
       const element = document.createElement('div');
       element.className = 'marker';
       element.style.backgroundImage = `url('${marker.image_url}')`;
-      element.style.backgroundSize = 'contain';
-      element.style.width = '25px';
-      element.style.height = '25px';
+      element.style.width = '32px';
+      element.style.height = '48px';
 
       new mapboxgl.Marker(element)
         .setLngLat([ marker.lng, marker.lat ])
