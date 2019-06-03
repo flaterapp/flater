e05345c2075f193fc2111f6f5da39314b6e42917
Pierre M'baga <pierrembaga@gmail.com>
2019-06-03 17:08:22 +0200

    Hover on each item related to Map box

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -3,6 +3,37 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 // import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder'; // <-- Not needed (debug from Geocoding-242 Jpheos)
 // import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'; // <-- Not needed (debug from Geocoding-242 Jpheos)
 
+const markersJs = {}
+
+const showMarkerJs = (id) => {
+  const markerJs = markersJs[id]
+  const popup = markerJs.getPopup()
+  if (popup.isOpen() == false)
+    markerJs.togglePopup()
+}
+
+const hideMarkerJs = (id) => {
+  const markerJs = markersJs[id]
+  const popup = markerJs.getPopup()
+  if (popup.isOpen() == true)
+    markerJs.togglePopup()
+}
+
+const elementLiOver = (e) => {
+  showMarkerJs(e.currentTarget.dataset.flatId)
+}
+
+const elementLiOut = (e) => {
+  hideMarkerJs(e.currentTarget.dataset.flatId)
+}
+
+const initFlatBinding = () => {
+  document.querySelectorAll(".list-group li").forEach((element) => {
+    element.addEventListener("mouseover", elementLiOver)
+    element.addEventListener("mouseout", elementLiOut)
+  })
+}
+
 const initMapbox = () => {
   // SELECT MAP
   const mapElement = document.getElementById('map');
@@ -11,9 +42,9 @@ const initMapbox = () => {
   const fitMapToMarkers = (map, markers) => {
     const bounds = new mapboxgl.LngLatBounds();
     markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-    map.fitBounds(bounds, { padding: 200, maxZoom: 15 });
+    map.fitBounds(bounds, { padding:200, maxZoom: 16 });
   };
-
+""
   // BUILD MAP // only build a map if there's a div#map to inject into
   if (mapElement) {
     // MAPBOX
@@ -24,22 +55,28 @@ const initMapbox = () => {
     });
     // DEFINE MARKERS
     const markers = JSON.parse(mapElement.dataset.markers);
-    markers.forEach((marker) => {
-      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+    markers.forEach((markerData) => {
+      const popup = new mapboxgl.Popup()
+      .setHTML(markerData.infoWindow);
       const element = document.createElement('div');
       element.className = 'marker';
-      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundImage = `url('${markerData.image_url}')`;
       element.style.width = '32px';
       element.style.height = '48px';
 
-      new mapboxgl.Marker(element)
-        .setLngLat([ marker.lng, marker.lat ])
+      let markerJs = new mapboxgl.Marker(element)
+
+      markerJs.setLngLat([ markerData.lng, markerData.lat ])
         .setPopup(popup)
         .addTo(map);
+      markersJs[markerData.id] = markerJs
     });
+    console.log(markersJs)
     // DISPLAY MAP
     fitMapToMarkers(map, markers);
   };
-};
+  initFlatBinding();
+}
+
 
 export { initMapbox };
