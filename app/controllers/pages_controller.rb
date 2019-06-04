835962ae8f7366f931b6ac6482f8b19d26a09a34
Raphael Massonneau <raphael.massonneau@gmail.com>
2019-06-04 18:09:39 +0200

    TWEAK my visits page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,5 +10,6 @@ class PagesController < ApplicationController
   def my_visits
     @all_flats = current_user.flats.order('id ASC')
     @flats_with_pending_rentals = @all_flats.joins(:rentals).where(rentals: { pending: true }).uniq
+    params[:format] == "?new" ? @new_visit = true : @new_visit = false
   end
 end
